1b8319348d10d217f2676df538f6c62f15cec107
Stéphane MEYER <tigerlost@me.com>
2022-02-21 19:40:37 +0100

    removed unused functions.

--- src/lib/curse.sh
+++ src/lib/curse.sh
@@ -22,7 +22,7 @@
 #
 # CURSE
 # C : 2022/02/17
-# M : 2022/02/18
+# M : 2022/02/21
 # D : Terminal colors and cursor manipulating functions.
 
 # colors
@@ -32,26 +32,6 @@ YLW='\x1b[30m\x1b[43m'; export YLW
 DIM='\x1b[2m'; export DIM
 OFF='\x1b[0m'; export OFF
 
-locate() {
-  # move cursor position.
-  local y x
-  y="$1"; x="$2"
-  printf '\x1b[%d;%dH' $((y)) $((x))
-}
-
-lecho() {
-  # move cursor position and print text.
-  local y x text
-  y="$1"; x="$2"; text="$3"
-  locate $((y)) $((x))
-  printf "%s" "$text"
-}
-
-clrscr() {
-  # clear the screen.
-  printf '\x1b[2J\x1b[H'
-}
-
 clrtoeol() {
   # clear from current position to the end of the line.
   printf '\x1b[K'
@@ -60,21 +40,6 @@ clrtoeol() {
 clrtoeos() {
   # clear from current position to the end of the screen.
   printf '\x1b[0J'
-}
-
-clrline() {
-  # clear line at current position.
-  printf '\x1b[2K'
-}
-
-hidecursor() {
-  # hide cursor.
-  printf '\x1b[?25l'
-}
-
-showcursor() {
-  # show cursor.
-  printf '\x1b[?25h'
 }
 
 savecursor() {
